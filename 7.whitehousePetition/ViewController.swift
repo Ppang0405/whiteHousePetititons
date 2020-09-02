ff91effeb10fdf7672dd2a5b8ade0374b3f0893e
Truong Nguyen <nguyentruong.xyz0405@gmail.com>
2020-09-02 18:33:30 +0700

    Initial Commit

---
+++ 7.whitehousePetition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  7.whitehousePetition
+//
+//  Created by Truong Nguyen on 9/2/20.
+//  Copyright © 2020 Truong Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
